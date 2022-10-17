c953951ebcc8c10ca919cebdaf66ed9f6144fa79
Denis Cheung <d.s.h.cheung@gmail.com>
2022-10-17 14:55:10 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const fs = require("fs")
 const axios = require('axios')
 const { JSDOM } = require('jsdom')
 const { Readability } = require('@mozilla/readability')
 const { extract } = require('article-parser')
 const morgan = require('morgan')
 const PCR = require("puppeteer-chromium-resolver")
+const URL = require('url').URL
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,6 +14,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(morgan('dev'))
+
+const dirname = `${__dirname}/tmp`
+if (!fs.existsSync(dirname)) fs.mkdirSync(dirname)
 
 // Readability
 app.get("/article1", async (req, res) => {
@@ -49,7 +54,7 @@ app.get("/article3", async (req, res) => {
     const stats = await PCR({
       revision: "",
       detectionPath: "",
-      folderName: "tmp/.chromium-browser-snapshots",
+      folderName: `${__dirname}/tmp/.chromium-browser-snapshots`,
       defaultHosts: ["https://storage.googleapis.com", "https://npm.taobao.org/mirrors"],
       hosts: [],
       cacheRevisions: 2,
@@ -59,24 +64,24 @@ app.get("/article3", async (req, res) => {
 
     console.log('stats', stats)
 
-    const browser3 = await stats.puppeteer.launch({
-      headless: false,
-      args: ["--no-sandbox"],
-      executablePath: stats.executablePath,
-      // args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process'],
-    })
+    // const browser3 = await stats.puppeteer.launch({
+    //   headless: false,
+    //   args: ["--no-sandbox"],
+    //   executablePath: stats.executablePath,
+    //   // args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process'],
+    // })
 
-    const page3 = await browser3.newPage()
-    console.log('page3', page3)
-    await page3.goto(url)
-    const html3 = await page3.evaluate(() => document.body.innerHTML)
-    console.log('html3', html3)
-    const doc3 = new JSDOM(html3)
-    console.log('doc3', doc3)
-    const article3 = new Readability(doc3.window.document).parse()
-    console.log('article3', article3)
-    await browser3.close()
-    res.status(200).json(article3)
+    // const page3 = await browser3.newPage()
+    // console.log('page3', page3)
+    // await page3.goto(url)
+    // const html3 = await page3.evaluate(() => document.body.innerHTML)
+    // console.log('html3', html3)
+    // const doc3 = new JSDOM(html3)
+    // console.log('doc3', doc3)
+    // const article3 = new Readability(doc3.window.document).parse()
+    // console.log('article3', article3)
+    // await browser3.close()
+    // res.status(200).json(article3)
   } catch (err) {
     console.log(err)
     res.status(400).json(err)
